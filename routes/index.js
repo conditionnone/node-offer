771d38db1ea7f396911c53a3c8f48058865cde67
lin jie <390380780@qq.com>
2017-10-30 01:06:20 +0800

    数据库相关
    数据库相关改动

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '霹雳人物汇' });
 });
 
 module.exports = router;
